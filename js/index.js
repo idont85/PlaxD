f2744ae47f5be89490b8c6f40df3dd8aab5f2a79
idont85 <99503136+idont85@users.noreply.github.com>
2023-12-06 12:18:35 -0500

    major update

---
+++ js/index.js
@@ -0,0 +1,51 @@
+function moveOver() {
+    var link = 'https://beatbump.io'
+    window.location.href = link
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
+
+function changeTab() {
+    var link, favicon;
+    link = document.getElementById('tab_pic_input').value;
+    setCookie("tab_name", link, 365);
+    alert("done")
+
+}
+
+function tempCheck() {
+    var current = getCookie("tab_pic_input");
+    alert(current)
+}
